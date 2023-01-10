0ff0528cf4022d9813d69d1995997c4aa2a65821
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2023-01-10 12:43:13 +0300

    Initial Commit

---
+++ NewsApp-PinsteresLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp-PinsteresLayout
+//
+//  Created by YILDIRIM on 10.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
